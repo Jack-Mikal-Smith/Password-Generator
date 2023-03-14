5a07053523f05b426b4aed69f09d0938c7ec22c8
Jack Smith <jack_smith21554@yahoo.com>
2023-03-13 21:31:19 -0400

    Added working outline in readme doc; includes requirements and description.  Added psydeuo-code to java script file explaining what functions and variables need to be added; set-up general structure of java script code.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Password-Generator
+
+# Generates a random password based on several user criteria.
+    Character length
+    Uppercase characters
+    Lowercase characters
+    Special characters
+    Numeric characters
+
+# When the button is clicked on, the application will prompt users with the above mentioned criteria and create a password based on the inputted character length and selected character groups.
+
+# Application will alert users when their inputs are invaild.
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Create character arrays to pull from
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +14,18 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Write generate password function
+  // Create and empty array to store character sets
+  // Prompt user to input password length; make sure input is between 8-128 characters
+
+// Create confirm prompts for each character set and assign them as a variable
+
+// Make sure users select at least one character set
+
+// Add selected character sets to array of possible characters
+
+// For the inputted password length, pick a random character from possible characters array 
+
+// Display randomly generated password
+
